c42d973db51d7f7161f8e6a5c6d6b532c63a2b3a
wadams-dev <wadams.dev@gmail.com>
2020-07-22 20:46:45 -0400

    add new project

---
+++ src/emojipedia.js
@@ -0,0 +1,47 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "😌 ",
+      name: "Relieved Face",
+      meaning:
+        "Everything went well! Relieved smiley face. Is happy that something unpleasant is over without having caused any harm. Relaxed, thankful and free of worries."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "😎",
+      name: "Face With Sunglasses",
+      meaning:
+        "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+    },
+    {
+        id: 5,
+        emoji: "🤩",
+        name: "Smirking Face With Starry Eyes",
+        meaning:
+          "Is very excited about something and already full of anticipation. Overwhelmed and speechless after meeting someone you like a lot. Fascinated by the glory and the world of the stars."
+      },
+      {
+        id: 6,
+        emoji: "🥳 ",
+        name: "Face with party blower and party hat",
+        meaning:
+          "That was a wild party or “Let's celebrate“. No matter what joyful event is in view, it will be celebrated appropriately. May also stand for the carnival season."
+      }
+  ];
+  
+  export default emojipedia;
+  
